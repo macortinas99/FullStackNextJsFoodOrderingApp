91dbe1b6a334f585d36aaaf3dafa9cb6efffe26e
macortinas99 <matthewcortinas22@gmail.com>
2022-11-22 00:29:52 -0600

    finished alpha version of website

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -9,6 +9,8 @@ export default async function handler(req, res) {
 
   await dbConnect()
 
+  const token = cookies.token
+
   if (method === 'GET') {
     try {
       const products = await Product.findById(id)
@@ -18,17 +20,25 @@ export default async function handler(req, res) {
     }
   }
   if (method === 'PUT') {
+    if (!token || token !== process.env.token) {
+      return res.status(401).json('Not Authenticated')
+    }
     try {
-      const product = await Product.create(req.body)
+      const product = await Product.findByIdAndUpdate(id, req.body, {
+        new: true,
+      })
       res.status(201).json(product)
     } catch (err) {
       res.status(500).json(err)
     }
   }
   if (method === 'DELETE') {
+    if (!token || token !== process.env.token) {
+      return res.status(401).json('Not Authenticated')
+    }
     try {
-      const product = await Product.create(req.body)
-      res.status(201).json(product)
+      await Product.findByIdAndDelete(id)
+      res.status(201).json('The Product has been deleted!')
     } catch (err) {
       res.status(500).json(err)
     }
